4c15e033464141ad3db6383c0b5fc15cd37ddda8
Zayd Tawam <93412824+ZaydTawam@users.noreply.github.com>
2026-07-13 10:33:56 -0400

    Add: trapping-rain-water - submission-11

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-11.py
@@ -0,0 +1,36 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        areas = [0]*len(height)
+
+        l = 0
+        for r in range(len(height)):
+            if height[r] < height[l] or l == r:
+                continue
+            min_bar = min(height[l], height[r])
+            while min_bar and l < r - 1:
+                l += 1
+                areas[l] = min_bar - height[l]
+            l = r
+        
+        print(areas)
+
+        l = len(height) - 1
+
+        for r in range(len(height) - 1, -1, -1):
+            if height[r] < height[l] or l == r:
+                continue
+            min_bar = min(height[l], height[r])
+            while min_bar and l > r + 1:
+                l -= 1
+                areas[l] = max(areas[l], min_bar - height[l])
+            l = r
+
+        print(areas)
+
+        total_area = 0
+        for area in areas:
+            total_area += area
+
+        return total_area
+        
+
