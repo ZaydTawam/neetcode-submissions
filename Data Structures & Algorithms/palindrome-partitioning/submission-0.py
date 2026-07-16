603bc0b14df8999d40ba0b3f137edd4f51328704
Zayd Tawam <93412824+ZaydTawam@users.noreply.github.com>
2026-07-16 11:07:02 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        vaild_branches = []
+
+        def is_palindrome(start, end):
+            l, r = start, end
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        def backtrack(branch, index):
+            if index == len(s):
+                vaild_branches.append(branch.copy())
+            
+            for i in range(index, len(s)):
+                res = is_palindrome(index, i)
+                if i != index and not res:
+                    continue
+                branch.append(s[index:i + 1])
+                backtrack(branch, i + 1)
+                branch.pop()
+        
+        backtrack([], 0)
+        return vaild_branches
